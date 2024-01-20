50f912407c53d19bd1b5c80fe3e1f0fc5997fb3e
HariPattem <haripattem1269@gmail.com>
2024-01-20 22:00:01 +0530

    Adding the new file

--- Top_K_Frequent_Elements_347.java
+++ Top_K_Frequent_Elements_347.java
@@ -3,47 +3,39 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-class Top_K_Frequent_Elements_347 {
-    public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:nums){
-              if(map.containsKey(num)){
-                  int temp=map.get(num);
-                   map.put(num,++temp);
-              }else{
-                  map.put(num,1);
-              }
-        }
-       List<Integer> list[]=new List[nums.length+1];
-       
-             for(Map.Entry<Integer,Integer> m:map.entrySet()){
-                int temp=m.getValue();
-                if(list[temp]==null){
-                    list[temp]=new ArrayList<>();
-                }
-                list[temp].add(m.getKey());
-                 
-             }
-    
-            int arr[]=new int[k];
-            int count=0;
-            for(int i=nums.length;i>=0 && count<k;i--){
-                if(list[i]!=null){
-                    for(Integer num:list[i]){
-                       arr[count++]=num;
-                    }
-                }
-    
-            }
-        return arr;
-        }
-    public static void main(String args[]){
-      int arr[]={1,1,1,2,2,3};
-      int k=2;
-      int arr1[]=new Top_K_Frequent_Elements_347().topKFrequent(arr, k);
-        for(int num:arr1){
-      System.out.println(num);
-        }
-
+public class Top_K_Frequent_Elements_347 {
+ public int[] topKFrequent(int[] nums, int k) {
+    HashMap<Integer,Integer> map=new HashMap<>();
+    for(int num:nums){
+          if(map.containsKey(num)){
+              int temp=map.get(num);
+               map.put(num,++temp);
+          }else{
+              map.put(num,1);
+          }
     }
+   List<Integer> list[]=new List[nums.length+1];
+   
+         for(Map.Entry<Integer,Integer> m:map.entrySet()){
+            int temp=m.getValue();
+            if(list[temp]==null){
+                list[temp]=new ArrayList<>();
+            }
+            list[temp].add(m.getKey());
+             
+         }
+
+        int arr[]=new int[k];
+        int count=0;
+        for(int i=nums.length;i>=0 && count<k;i--){
+            if(list[i]!=null){
+                for(Integer num:list[i]){
+                   arr[count++]=num;
+                }
+            }
+
+        }
+    return arr;
+    }
+    
 }
