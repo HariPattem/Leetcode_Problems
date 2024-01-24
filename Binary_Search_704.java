692478b366fa8f48485e9cd4dde0f3fcbff9d293
HariPattem <haripattem1269@gmail.com>
2024-01-25 00:37:58 +0530

    Adding Binary search problem

---
+++ Binary_Search_704.java
@@ -0,0 +1,19 @@
+public class Binary_Search_704 {
+    public int search(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        int mid=0;
+        while(left<=right){
+           mid=(left+right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>target){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
+        return -1;
+    }
+}
