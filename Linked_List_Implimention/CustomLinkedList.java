2805704faeeea45f34449ddd20606a0ee2d1833f
HariPattem <haripattem1269@gmail.com>
2024-02-20 12:46:04 +0530

    Adding the Linked List Implementation

---
+++ Linked_List_Implimention/CustomLinkedList.java
@@ -0,0 +1,92 @@
+
+
+public class CustomLinkedList<T> {
+   private Node<T> head;
+   private int capacity=0;
+    public void insert(T val){
+      capacity++;
+        Node<T> node=new Node<>();
+        node.data=val;      
+      if(head==null){
+        head=node;
+      }else{
+        Node<T> tempHead=head;
+        while(tempHead.next!=null){
+            tempHead=tempHead.next;
+        }
+        tempHead.next=node;
+      }
+    }
+
+
+    public void insertAt(int index,T val){
+      capacity++;
+      Node<T> node=new Node<>();
+      node.data=val;
+
+      if(index==0){
+        node.next=head;
+        head=node;
+       return;
+      }
+      Node<T> n=head;
+      for(int i=0;i<index-1;i++){
+        n=n.next;
+      }
+      node.next=n.next;
+      n.next=node; 
+    }
+    
+    public void remove(int index){
+      if(index>index){
+        throw new IndexOutOfBoundsException();
+      }
+      Node<T> n=head;
+      for(int i=0;i<index-1;i++){
+          n=n.next;
+      }
+      
+      n.next=n.next.next;
+    
+     capacity--;
+    }
+
+
+    public void insertAtEnd(T val){
+       insert(val);
+    }
+    
+    public void insertAtStart(T val){
+      Node<T> node=new Node<>();
+      node.data=val;
+      node.next=head;
+      head=node;
+   }
+
+
+  public boolean contains(T val){
+    Node<T> node=head;
+   while(node.next!=null){
+    if(node.data.equals(val)){
+        return true;
+    }
+    node=node.next;
+   }
+   if(node.data.equals(val)){
+      return true;
+   }
+    return false;
+  }
+
+    public void show(){
+        Node<T> n=head;
+        if(n!=null){
+          System.out.print("[ ");
+        while(n!=null){
+        System.out.print(n.data+" ");
+        n=n.next;
+    }
+   System.out.println("]");
+  }
+   }
+}
---
+++ Linked_List_Implimention/LinkedListRunner.java
@@ -0,0 +1,40 @@
+public class LinkedListRunner {
+    public static void main(String []args){
+      // CustomLinkedList<Integer> list=new CustomLinkedList<>();
+      // list.insert(20);
+      // list.insert(30);
+      // list.show();
+      // list.insert(50);
+      // list.insert(60);
+      // list.show();
+      // list.insertAt(3, 20);
+      // list.show();
+      // list.insertAt(0,100);
+      // list.show();
+      // list.insertAt(1,120);
+      // list.show();
+      // list.insertAt(6,120);
+      // list.show();
+      // list.insertAt(8,120);
+      // list.show();
+      // list.remove(6);
+      // list.show();
+      // list.remove(7);
+      // list.show();
+      // list.remove(4);
+      // list.show();
+      // list.insertAtEnd(10);
+      // list.insertAtStart(1000);
+      // list.show();
+      // System.out.println(list.contains(10));
+      CustomLinkedList<String> list=new CustomLinkedList<>();
+      list.insert("Hari");
+      list.insert("H");
+      list.insert("a");
+      list.insert("r");
+      list.insert("i");
+      list.insert("k");
+      list.insert("i");
+      list.show();
+    }
+}
